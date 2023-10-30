4f80ab1e0c35c8243a9b8fc99ec31c3249a8a3a5
meh <meh@IT5>
2023-10-30 14:15:43 +0800

    +

--- DZ-4m-27l/Program.cs
+++ DZ-4m-27l/Program.cs
@@ -11,28 +11,35 @@ internal class Program
         Start(Examination);
         Console.ReadKey();
     }
-    static Task Examination(Task task)
+    static async Task<Task> Examination(Task task)
     {
         var answer = Console.ReadKey().KeyChar;
+
         Console.WriteLine();
         if (answer != 65)
         {
             if (!task.IsCompletedSuccessfully)
             {
-                Console.WriteLine($"Ещё скачивается");
+                Console.WriteLine($"Ещё скачивается{task.Id}");
             }
             else
             {
-                Console.WriteLine($"Загрузка завершена");
+                Console.WriteLine($"Загрузка завершена{task.Id}");
             }
 
             Examination(task);
         }
+
         return task;
     }
+
+
+
     async static Task Start(StartExamDelegate Examination)
 
-    {        
+    {
+
+
         //Создание объекта подписка на события
         var imageDownloader = new ImageDownloader();
         imageDownloader.ImageStarted += Message.MessageStart;
@@ -45,27 +52,36 @@ internal class Program
         var task = imageDownloader.DownloaderAsync();
 
 
-
         //Конец выполнения
-        Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
+
+        Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");        
+
         Examination(task);
+
         Console.Clear();
         Console.WriteLine("Нажмите любую кнопку");
         Console.ReadKey();
         Console.Clear();
-
-
         Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
+
+
         List<Task> tasks = new List<Task>();
         for (int i = 0; i < 10; i++)
         {
             tasks.Add(imageDownloader.DownloaderAsync());
         }
+
         foreach (var item in tasks)
         {
-           await Examination(item);
+            Task.Factory.StartNew(async () =>
+            {
+                await Examination(item);
+            });
         }
-        await Task.WhenAll(tasks);
+        do
+        {
+
+        } while (!Task.WhenAll(tasks).IsCompleted);
     }
 }
 
